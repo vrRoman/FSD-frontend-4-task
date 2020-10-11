e36bc767d827b4e6a0dc3256632b2c2cb9d33054
Roman Valiev <valromrin@mail.ru>
2020-10-11 18:50:55 +0300

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path')
+const webpack = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+
+  entry: {
+    'index': '@index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'slider.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: true,
+              reloadAll: true
+            }
+          },
+          'css-loader',
+          'resolve-url-loader',
+          'sass-loader'
+        ],
+      },
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'tests'),
+    compress: true,
+    port: 9000,
+    hot: true
+  },
+
+  optimization: {
+    minimize: false
+  }
+}
