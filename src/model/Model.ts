43989ee9c781bb1353252f58462b4f08522a88b3
Roman Valiev <valromrin@mail.ru>
2020-10-18 10:13:37 +0300

    Added change value, addStepsToValue, checkAndFixValue to model

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -8,7 +8,19 @@ interface Options {
   min: number
 }
 
-export default class Model {
+interface IModel {
+  value: Value
+  range: boolean
+  stepSize: number
+  max: number
+  min: number
+
+  changeValue(newValue: Value): Value
+  addStepsToValue(numOfSteps: number, valueNumber: 0 | 1): Value
+  checkAndFixValue(): Value
+}
+
+export default class Model implements IModel {
   value: Value
   range: boolean
   stepSize: number
@@ -21,6 +33,15 @@ export default class Model {
 
     this.value = options.value;
 
+    // Если макс. значение > мин., то поменять местами.
+    if (this.max < this.min) {
+      [this.max, this.min] = [this.min, this.max];
+    }
+
+    // Если значение - одно число и это диапазон, то значение становится
+    // массивом с двумя одинаковыми значениями.
+    // Если значение - массив и это не диапазон, то значением становится
+    // первый элемент массива.
     this.range = options.range;
     if (this.range) {
       if (typeof this.value === 'number') {
@@ -30,68 +51,88 @@ export default class Model {
       [this.value] = this.value;
     }
 
-    // if (this.max > this.min) {
-    //   if (typeof this.value === 'number') {
-    //     if (this.value > this.max) {
-    //       this.value = this.max;
-    //     }
-    //     if (this.value < this.min) {
-    //       this.value = this.min;
-    //     }
-    //   } else {
-    //     if (this.value[1] > this.max) {
-    //       this.value[1] = this.max;
-    //     }
-    //     if (this.value[0] < this.min) {
-    //       this.value[0] = this.min;
-    //     }
-    //   }
-    // } else if (typeof this.value === 'number') {
-    //   if (this.value > this.min) {
-    //     this.value = this.min;
-    //   }
-    //   if (this.value < this.max) {
-    //     this.value = this.max;
-    //   }
-    // } else {
-    //   if (this.value[1] > this.min) {
-    //     this.value[1] = this.min;
-    //   }
-    //   if (this.value[0] < this.max) {
-    //     this.value[0] = this.max;
-    //   }
-    // }
+    // Если это диапазон и первое значение больше второго, поменять их местами.
+    // Если значения больше максимального, то
+    // приравнять с максимальным, и наоборот для минимального.
+    this.checkAndFixValue();
 
+    // Если размер шага <= 0, то он равен 1.
+    // Если размер шага > наибольшего диапазона значений, то он равняется
+    // разнице максимального значения и минимального.
     this.stepSize = options.stepSize;
-    if (this.stepSize < 1) {
+    if (this.stepSize <= 0) {
       this.stepSize = 1;
-    } else if (this.stepSize > this.max - this.min) {
+    }
+    if (this.stepSize > this.max - this.min) {
       this.stepSize = this.max - this.min;
     }
   }
 
+  // Изменяет текущее значение.
+  // Если значение - число, а новое - массив, то берется первое число массива.
+  // Если значение - массив, а новое - число, то значение = массиву с двумя
+  // одинаковыми значениями.
   changeValue(newValue: Value): Value {
-    if (typeof this.value !== 'number') {
-      if (typeof newValue !== 'number') {
+    if (typeof this.value === 'number') {
+      if (typeof newValue === 'number') {
         this.value = newValue;
       } else {
-        this.value = [newValue, newValue];
+        [this.value] = newValue;
       }
-    } else if (typeof newValue === 'number') {
+    } else if (typeof newValue !== 'number') {
       this.value = newValue;
     } else {
-      [this.value] = newValue;
+      this.value = [newValue, newValue];
     }
+
+    this.checkAndFixValue();
 
     return this.value;
   }
 
-  addStepsToValue(numOfSteps: number, valueNumber: 0 | 1): Value {
-    if (typeof this.value !== 'number') {
-      this.value[valueNumber] += numOfSteps * this.stepSize;
-    } else {
+  // Добавляет указанное количество шагов к нужному значению(если не
+  // диапазон или нужно большее значение, то указывать не обязательно)
+  addStepsToValue(numOfSteps: number, valueNumber: 0 | 1 = 1): Value {
+    if (typeof this.value === 'number') {
       this.value += numOfSteps * this.stepSize;
+    } else {
+      this.value[valueNumber] += numOfSteps * this.stepSize;
     }
+
+    this.checkAndFixValue();
+
+    return this.value;
+  }
+
+  // Если это диапазон и первое значение больше второго, поменять их местами.
+  // Если значения больше максимального, то
+  // приравнять с максимальным, и наоборот для минимального.
+  checkAndFixValue(): Value {
+    if (typeof this.value !== 'number') {
+      if (this.value[0] > this.value[1]) {
+        this.value = [this.value[1], this.value[0]];
+      }
+    }
+
+    if (typeof this.value === 'number') {
+      if (this.value > this.max) {
+        this.value = this.max;
+      } else if (this.value < this.min) {
+        this.value = this.min;
+      }
+    } else {
+      if (this.value[1] > this.max) {
+        this.value[1] = this.max;
+      } else if (this.value[1] < this.min) {
+        this.value[1] = this.min;
+      }
+      if (this.value[0] < this.min) {
+        this.value[0] = this.min;
+      } else if (this.value[0] > this.max) {
+        this.value[0] = this.max;
+      }
+    }
+
     return this.value;
   }
 }
--- test/model-test.ts
+++ test/model-test.ts
@@ -2,29 +2,43 @@ import { assert } from 'chai';
 import Model from '../src/model/Model';
 
 describe('model', () => {
-  it('model value', () => {
+  it('model options', () => {
     const modelTrueRange = new Model({
       value: 12,
       range: true,
       stepSize: 1,
       min: 0,
-      max: 15,
+      max: 10,
     });
 
     // @ts-ignore
-    assert.strictEqual(modelTrueRange.value[0], 12);
+    assert.strictEqual(modelTrueRange.value[0], 10);
     // @ts-ignore
-    assert.strictEqual(modelTrueRange.value[1], 12);
+    assert.strictEqual(modelTrueRange.value[1], 10);
 
     const modelFalseRange = new Model({
       value: [12, 12],
       range: false,
       stepSize: 1,
       min: 0,
-      max: 15,
+      max: 10,
     });
 
-    assert.strictEqual(modelFalseRange.value, 12);
+    assert.strictEqual(modelFalseRange.value, 10);
+
+    const model = new Model({
+      value: [60, -100],
+      range: true,
+      stepSize: 100,
+      min: 50,
+      max: -15,
+    });
+    assert.strictEqual(model.min, -15);
+    assert.strictEqual(model.max, 50);
+    // @ts-ignore
+    assert.strictEqual(model.value[0], -15);
+    // @ts-ignore
+    assert.strictEqual(model.value[1], 50);
   });
 
   it('model stepSize', () => {
@@ -50,31 +64,32 @@ describe('model', () => {
   });
 
   it('change value and addStepsToValue', () => {
-    const modelTrueRange = new Model({
+    const model = new Model({
       value: [12, 14],
       range: true,
       stepSize: 1,
       min: 0,
-      max: 10,
+      max: 15,
     });
     // @ts-ignore
-    assert.strictEqual(modelTrueRange.changeValue(10)[0], 10);
+    assert.strictEqual(model.changeValue(10)[0], 10);
     // @ts-ignore
-    assert.strictEqual(modelTrueRange.changeValue(10)[1], 10);
+    assert.strictEqual(model.changeValue(10)[1], 10);
 
     // @ts-ignore
-    assert.strictEqual(modelTrueRange.changeValue([10, 15])[0], 10);
+    assert.strictEqual(model.changeValue([10, 15])[0], 10);
     // @ts-ignore
-    assert.strictEqual(modelTrueRange.changeValue([10, 15])[1], 15);
+    assert.strictEqual(model.changeValue([10, -10])[1], 10);
+    // @ts-ignore
+    assert.strictEqual(model.changeValue([10, -10])[0], 0);
 
-    // const modelFalseRange = new Model({
-    //   value: 12,
-    //   range: false,
-    //   stepSize: 1,
-    //   min: 0,
-    //   max: 10,
-    // });
-    // assert.strictEqual(modelFalseRange.changeValue(10), 10);
-    // assert.strictEqual(modelFalseRange.changeValue([10, 15]), 10);
+    model.changeValue([12, 14]);
+
+    // @ts-ignore
+    assert.strictEqual(model.addStepsToValue(10)[1], 15);
+
+    model.changeValue([-100, 14]);
+    // @ts-ignore
+    assert.strictEqual(model.addStepsToValue(10, 0)[0], 10);
   });
 });
