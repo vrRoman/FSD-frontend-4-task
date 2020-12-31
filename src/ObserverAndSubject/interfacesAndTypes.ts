d127d8b7027fe1b0dd42cc9c2bb63015b99dc867
Roman Valiev <valromrin@mail.ru>
2020-12-31 18:07:29 +0300

    Changed type of action.type

--- src/ObserverAndSubject/interfacesAndTypes.ts
+++ src/ObserverAndSubject/interfacesAndTypes.ts
@@ -1,10 +1,13 @@
 import { Value } from '../modules/Model/interfacesAndTypes';
 
 
+type actionModelType = 'UPDATE_VALUE' | 'UPDATE_RANGE' | 'UPDATE_MIN-MAX' | 'UPDATE_STEPSIZE';
+type actionViewType = | 'UPDATE_LENGTH' | 'UPDATE_VERTICAL' | 'UPDATE_RESPONSIVE' | 'UPDATE_TOOLTIP'
+  | 'UPDATE_STEPSINFO-SETTINGS' | 'UPDATE_VALUEINFO' | 'UPDATE_USEKEYBOARD' | 'UPDATE_STEPSINFO-INTERACTIVITY'
+
 type SubjectAction = {
-  type: 'UPDATE_VALUE' | 'UPDATE_RANGE' | 'UPDATE_MIN-MAX' | 'UPDATE_STEPSIZE'
-    | 'UPDATE_LENGTH' | 'UPDATE_VERTICAL' | 'UPDATE_RESPONSIVE' | 'UPDATE_TOOLTIP'
-    | 'UPDATE_STEPSINFO-SETTINGS' | 'UPDATE_VALUEINFO' | 'UPDATE_USEKEYBOARD' | 'UPDATE_STEPSINFO-INTERACTIVITY'
+  type: actionModelType | actionViewType
+
   updatedProps?: {
     value?: Value
     range?: boolean
