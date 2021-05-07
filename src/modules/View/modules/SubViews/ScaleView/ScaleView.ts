83912da2ccea781729979aa1f6bb0a773ab505fc
Roman Valiev <valromrin@mail.ru>
2021-05-07 14:44:03 +0300

    fix bug when values do not correspond to the height #4

--- src/modules/View/modules/SubViews/ScaleView/ScaleView.ts
+++ src/modules/View/modules/SubViews/ScaleView/ScaleView.ts
@@ -34,6 +34,7 @@ class ScaleView implements IScaleView {
         const length = this.viewModel.getLengthInPx();
         if (length) {
           const scale = document.createElement('div');
+          scale.style.position = 'absolute';
           this.target.appendChild(scale);
 
           const { scaleClass } = this.viewModel.getClasses();
--- src/modules/View/modules/View/View.ts
+++ src/modules/View/modules/View/View.ts
@@ -307,6 +307,7 @@ class View extends Observer implements IView {
         if (this.sliderContainerView) this.sliderContainerView.updateVertical();
         if (this.barView) this.barView.updateBar();
         if (this.barView) this.barView.updateProgressBar();
+        if (this.barView) this.viewModel.setLengthInPx(this.barView.getOffsetLength() || 0);
         if (this.thumbView) this.thumbView.update();
         if (this.scaleView) this.scaleView.updateVertical();
         break;
