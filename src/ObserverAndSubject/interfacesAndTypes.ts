0e0290cd4a6a409d2cdcfe9b1fa48be47f80af5a
Roman Valiev <valromrin@mail.ru>
2021-05-26 11:05:30 +0300

    change type of actions

--- src/ObserverAndSubject/interfacesAndTypes.ts
+++ src/ObserverAndSubject/interfacesAndTypes.ts
@@ -3,7 +3,7 @@ import { ModelDataPartial } from '../modules/Model/interfacesAndTypes';
 type actionModelType = 'UPDATE_VALUE' | 'UPDATE_IS-RANGE' | 'UPDATE_MIN-MAX' | 'UPDATE_STEP-SIZE';
 type actionViewType = 'UPDATE_LENGTH' | 'UPDATE_IS-VERTICAL' | 'UPDATE_HAS-TOOLTIP'
   | 'UPDATE_HAS-SCALE' | 'UPDATE_SCALE-VALUE' | 'UPDATE_IS-SCALE-CLICKABLE' | 'UPDATE_IS-BAR-CLICKABLE'
-  | 'UPDATE_HAS-VALUE-INFO' | 'UPDATE_USE-KEYBOARD'
+  | 'UPDATE_HAS-VALUE-INFO' | 'UPDATE_USE-KEYBOARD' | 'UPDATE_MODEL-DATA' | 'UPDATE_LENGTH-IN-PX'
 
 type SubjectAction = {
   type: actionModelType | actionViewType
