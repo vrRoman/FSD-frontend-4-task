a13f1142108a22f2591dbbec567027235beb9d0c
Roman Valiev <valromrin@mail.ru>
2021-06-14 19:54:25 +0300

    fix eslint errors

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -5,6 +5,7 @@ import './blocks/slider-container/init';
 const $sliderWithOnChange = $('.js-slider3');
 $sliderWithOnChange.slider('changeOptions', {
   onChange() {
+    // eslint-disable-next-line no-console
     console.log($sliderWithOnChange.slider('value'));
   },
 });
