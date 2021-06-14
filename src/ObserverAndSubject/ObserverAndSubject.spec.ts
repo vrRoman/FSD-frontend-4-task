d14a455c3bc048d69bed0ab53b35457af7933ff3
Roman Valiev <valromrin@mail.ru>
2021-06-14 19:39:25 +0300

    add tests for Observer and Subject #6

---
+++ src/ObserverAndSubject/ObserverAndSubject.spec.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars,
+                  no-unused-vars,
+                  class-methods-use-this,
+                  no-useless-constructor,
+                  max-classes-per-file */
+
+import Observer from './Observer';
+import Subject from './Subject';
+import { IObserver, ISubject, SubjectAction } from './interfacesAndTypes';
+
+const createObserver = (subject: ISubject) => new (class extends Observer {
+  constructor(testingSubject: ISubject) {
+    super(testingSubject);
+  }
+
+  update(action: SubjectAction) {}
+})(subject);
+
+let subject: ISubject;
+let observer: IObserver;
+beforeEach(() => {
+  subject = new (class extends Subject {
+    constructor() {
+      super();
+    }
+  })();
+  observer = createObserver(subject);
+});
+
+test('Subject.getObservers should return array of current observers', () => {
+  expect(subject.getObservers()[0]).toBe(observer);
+});
+
+test('Subject.unsubscribe should remove observers', () => {
+  subject.unsubscribe(observer);
+  expect(subject.getObservers().length).toBe(0);
+});
