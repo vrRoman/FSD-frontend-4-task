cf210e1c5690b08b473ce02c596144aa2d45b7d7
Roman Valiev <roman.veyl@yandex.ru>
2021-08-19 12:49:26 +0300

    fix absolute paths in jest config #9

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,11 @@
 export default {
   clearMocks: true,
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
+  roots: ['src'],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^Model$': '<rootDir>/src/modules/Model',
+    '^View$': '<rootDir>/src/modules/View',
+    '^Presenter$': '<rootDir>/src/modules/Presenter',
+  },
 };
